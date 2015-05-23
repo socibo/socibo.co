4e70419e459a5f0d67b802dc09e85538206cce38
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2015-05-23 15:33:10 +0300

    trying to use proxy-http

--- server/app.js
+++ server/app.js
@@ -9,6 +9,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var mongoose = require('mongoose');
+var httpProxy = require('http-proxy');
+var https = require('https');
 var config = require('./config/environment');
 
 // Connect to database
@@ -17,8 +19,22 @@ mongoose.connect(config.mongo.uri, config.mongo.options);
 // Populate DB with sample data
 if(config.seedDB) { require('./config/seed'); }
 
+var options=require('proxy-by-url')({
+	'/github': { port: 80, host: 'github.com' },
+	'/nodejitsu': { port: 80, host: 'nodejitsu.com' },
+	'/localstuff': { port: 8080, host: 'localhost' }
+    })
+
+//
+// Just set up your options...
+//
+//
+// ...and then pass them in when you create your proxy.
+//
+var proxyServer = httpProxy.createServer(options).listen(9001);
 // Setup server
 var app = express();
+
 var server = require('http').createServer(app);
 var socketio = require('socket.io')(server, {
   serveClient: (config.env === 'production') ? false : true,
@@ -27,6 +43,8 @@ var socketio = require('socket.io')(server, {
 require('./config/socketio')(socketio);
 require('./config/express')(app);
 require('./routes')(app);
+
+
 
 // Start server
 server.listen(config.port, config.ip, function () {
